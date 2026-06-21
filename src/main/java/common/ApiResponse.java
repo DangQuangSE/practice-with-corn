93c89232c2802763c8b323c4c0b80062e01c1410
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-06-21 23:21:51 +0700

    feat: clean project

--- src/main/java/common/ApiResponse.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-import java.time.Instant;
-
-public record ApiResponse<T>(String message, T data, Instant timeStamp) {
-    public static <T> ApiResponse<T> of(String message, T data) {
-        return new ApiResponse<T>(message, data, Instant.now());
-    }
-}
